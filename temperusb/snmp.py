e023a99e18239cb7ef8ae25934a19f01ef367194
Philip Jay <phil@jay.id.au>
2016-04-18 23:39:06 +1000

    Fix linting
    A few in-line disables:
    - Allow "fd" as an argument name in _unbuffered_handle(fd) - I think
      most people would recognise that as "file descriptor"
    - Too few public methods on the classes - I didn't want to refactor that
      right now
    - Allow broad exceptions - since it's the current functionality, and I
      didn't want to refactor that right now
    
    Main difference is the use of 'snmp_pp' in place of 'pp' which is too
    short & not descriptive enough (in my opinion).  Trying to be a little
    more explicit.

--- temperusb/snmp.py
+++ temperusb/snmp.py
@@ -1,36 +1,41 @@
 # encoding: utf-8
-#
-# Run snmp_temper.py as a pass-persist module for NetSNMP.
-# See README.md for instructions.
-# 
-# Copyright 2012-2014 Philipp Adelt <info@philipp.adelt.net>
-#
-# This code is licensed under the GNU public license (GPL). See LICENSE.md for details.
+"""Run snmp_temper.py as a pass-persist module for NetSNMP.
+See README.md for instructions.
+
+Copyright 2012-2014 Philipp Adelt <info@philipp.adelt.net>
+
+This code is licensed under the GNU public license (GPL). See LICENSE.md for details.
+"""
 
 import os
 import sys
 import syslog
 import threading
 import snmp_passpersist as snmp
-from temperusb.temper import TemperHandler, TemperDevice
+from temperusb.temper import TemperHandler
 
 ERROR_TEMPERATURE = 9999
 
 
-def _unbuffered_handle(fd):
+def _unbuffered_handle(fd):  # pylint: disable=invalid-name
     return os.fdopen(fd.fileno(), 'w', 0)
 
 
-class LogWriter():
+class LogWriter(object):  # pylint: disable=too-few-public-methods
+    """Utility class for writing to syslog"""
     def __init__(self, ident='temper-python', facility=syslog.LOG_DAEMON):
         syslog.openlog(ident, 0, facility)
-    def write_log(self, message, prio=syslog.LOG_INFO):
+
+    @staticmethod
+    def write_log(message, prio=syslog.LOG_INFO):
+        """Write message to syslog"""
         syslog.syslog(prio, message)
 
-class Updater():
-    def __init__(self, pp, logger, testmode=False):
+class Updater(object):  # pylint: disable=too-few-public-methods
+    """Class to query the TEMPer USB device(s) and update SNMP OID's"""
+    def __init__(self, snmp_pp, logger, testmode=False):
         self.logger = logger
-        self.pp = pp
+        self.snmp_pp = snmp_pp
         self.testmode = testmode
         self.usb_lock = threading.Lock() # used to stop reinitialization interfering with update-thread
         self._initialize()
@@ -38,56 +43,60 @@ class Updater():
     def _initialize(self):
         with self.usb_lock:
             try:
-                self.th = TemperHandler()
-                self.devs = self.th.get_devices()
+                self.handler = TemperHandler()
+                self.devs = self.handler.get_devices()
                 self.logger.write_log('Found %i thermometer devices.' % len(self.devs))
                 for i, d in enumerate(self.devs):
-                    self.logger.write_log('Initial temperature of device #%i: %0.1f degree celsius' % (i, d.get_temperature()))
-            except Exception as e:
+                    self.logger.write_log(
+                        'Initial temperature of device #%i: %0.1f degree celsius' % (i, d.get_temperature())
+                    )
+            except Exception as e:  # pylint: disable=broad-except
                 self.logger.write_log('Exception while initializing: %s' % str(e))
 
     def _reinitialize(self):
         # Tries to close all known devices and starts over.
         self.logger.write_log('Reinitializing devices')
         with self.usb_lock:
-            for i,d in enumerate(self.devs):
+            for i, d in enumerate(self.devs):
                 try:
                     d.close()
-                except Exception as e:
-                    self.logger.write_log('Exception closing device #%i: %s' % (i, str(e))) 
+                except Exception as e:  # pylint: disable=broad-except
+                    self.logger.write_log('Exception closing device #%i: %s' % (i, str(e)))
         self._initialize()
 
     def update(self):
+        """Update the OID's with results"""
         if self.testmode:
             # APC Internal/Battery Temperature
-            self.pp.add_int('318.1.1.1.2.2.2.0', 99)
+            self.snmp_pp.add_int('318.1.1.1.2.2.2.0', 99)
             # Cisco devices temperature OIDs
-            self.pp.add_int('9.9.13.1.3.1.3.1', 97)
-            self.pp.add_int('9.9.13.1.3.1.3.2', 98)
-            self.pp.add_int('9.9.13.1.3.1.3.3', 99)
+            self.snmp_pp.add_int('9.9.13.1.3.1.3.1', 97)
+            self.snmp_pp.add_int('9.9.13.1.3.1.3.2', 98)
+            self.snmp_pp.add_int('9.9.13.1.3.1.3.3', 99)
         else:
             try:
                 with self.usb_lock:
                     temperatures = [d.get_temperature() for d in self.devs]
-                    self.pp.add_int('318.1.1.1.2.2.2.0', int(max(temperatures)))
+                    self.snmp_pp.add_int('318.1.1.1.2.2.2.0', int(max(temperatures)))
                     for i, temperature in enumerate(temperatures[:3]): # use max. first 3 devices
-                        self.pp.add_int('9.9.13.1.3.1.3.%i' % (i+1), int(temperature))
-            except Exception as e:
+                        self.snmp_pp.add_int('9.9.13.1.3.1.3.%i' % (i+1), int(temperature))
+            except Exception as e:  # pylint: disable=broad-except
                 self.logger.write_log('Exception while updating data: %s' % str(e))
                 # Report an exceptionally large temperature to set off all alarms.
                 # snmp_passpersist does not expose an API to remove an OID.
                 for oid in ('318.1.1.1.2.2.2.0', '9.9.13.1.3.1.3.1', '9.9.13.1.3.1.3.2', '9.9.13.1.3.1.3.3'):
-                    self.pp.add_int(oid, ERROR_TEMPERATURE)
+                    self.snmp_pp.add_int(oid, ERROR_TEMPERATURE)
                 self.logger.write_log('Starting reinitialize after error on update')
                 self._reinitialize()
 
 
 def main():
+    """Main method to start updating SNMP via the PassPersist protocol"""
     sys.stdout = _unbuffered_handle(sys.stdout)
-    pp = snmp.PassPersist(".1.3.6.1.4.1")
+    snmp_pp = snmp.PassPersist(".1.3.6.1.4.1")
     logger = LogWriter()
-    upd = Updater(pp, logger, testmode=('--testmode' in sys.argv))
-    pp.start(upd.update, 5) # update every 5s
+    upd = Updater(snmp_pp, logger, testmode=('--testmode' in sys.argv))
+    snmp_pp.start(upd.update, 5) # update every 5s
 
 
 if __name__ == '__main__':
